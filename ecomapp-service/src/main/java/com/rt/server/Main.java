834e8cb84297036b437629b05bacacc80deb3aca
rajeshatuce <rajeshatuce@gmail.com>
2019-02-14 22:53:57 +0530

    Add environment specific property file

--- ecomapp-service/src/main/java/com/rt/server/Main.java
+++ ecomapp-service/src/main/java/com/rt/server/Main.java
@@ -7,15 +7,44 @@ import com.rt.controller.EComAppController;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Main class for ecomapp
+ */
 public class Main {
 
-  private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-  public static void main(String[] args) {
+  /**
+   * Main method to be invoked from command line
+   */
+  public static void main(String[] args) throws Exception {
+    LOGGER.info("Starting ecomapp server on environment [ {} ]", args[0]);
+    Properties prop = loadEnvironmentPropertyFile(args[0]);
+    initializeVertx(Integer.parseInt(prop.getProperty("port")));
+    LOGGER.info("Server started successfully");
+  }
+
+  private static Properties loadEnvironmentPropertyFile(String arg) throws IOException {
+    String propertyFile = String.format("application_%s.properties", arg.toLowerCase());
+    Properties prop = new Properties();
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    InputStream stream = loader.getResourceAsStream(propertyFile);
+    prop.load(stream);
+    return prop;
+  }
+
+  /**
+   * Utility API to initialize vertx
+   */
+  private static void initializeVertx(int portNumber) {
     Vertx vertx = Vertx.vertx();
     vertx.runOnContext(aVoid -> {
 
@@ -23,7 +52,7 @@ public class Main {
       // The minimum config required is a package to inspect for JAX-RS endpoints
       vertx.getOrCreateContext().config()
           .put("jersey", new JsonObject()
-              .put("port", 8080)
+              .put("port", portNumber)
               .put("features", new JsonArray()
                   .add("org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature"))
               .put("instances", new JsonArray()
@@ -35,7 +64,6 @@ public class Main {
       JerseyServer server = locator.getService(JerseyServer.class);
       // Start the server
       server.start();
-      LOGGER.info("Server started successfully");
     });
   }
 
