295e5c1ebff3a73b437aed582fc653c0321093cd
rajeshatuce <rajeshatuce@gmail.com>
2019-02-17 00:00:28 +0530

    log statements

--- ecomapp-service/src/main/java/com/rt/controller/EComAppWebController.java
+++ ecomapp-service/src/main/java/com/rt/controller/EComAppWebController.java
@@ -1,6 +1,8 @@
 package com.rt.controller;
 
 import com.rt.util.EComAppCDNUrlBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EComAppWebController {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(EComAppWebController.class);
   private EComAppCDNUrlBuilder eComAppCDNUrlBuilder;
 
   @Autowired
@@ -23,6 +26,7 @@ public class EComAppWebController {
   @RequestMapping("/")
   public String getEComAppIndexPage(Model model) {
     eComAppCDNUrlBuilder.addCDNUrlToModel(model);
+    LOGGER.info("Rendering homepage");
     return "index";
   }
 }
